f4a64adc43b220cf3337ef2ae87ab35aef30425f
Veronika <nebesnav@gmail.com>
2020-06-23 12:00:30 +0300

    added code for likes

--- app.js
+++ app.js
@@ -1,11 +1,59 @@
-// let cart = document.body.firstElementChild.firstElementChild.firstElementChild.lastElementChild.firstElementChild.lastElementChild;
+let productCountEl = document.getElementById('products_count');
+console.log(productCountEl)
 
-let cart_count = document.querySelector('.product_count');
-let cart_img = document.querySelector('.cart')
-console.log(cart_count);
-console.log(cart_img);
-cart_img.addEventListener("click",numberChange);
-let i=1;
-function numberChange(){
-    cart_count.innerHTML = i++
+let addToCartButtons = document.querySelectorAll('.add_to_cart')
+console.log(addToCartButtons);
+
+for(let i=0; i<addToCartButtons.length; i++){
+    addToCartButtons[i].addEventListener("click", function(){
+        let prevProductsCount = +productCountEl.textContent;
+        productCountEl.textContent = prevProductsCount +1
+    })
 }
+
+
+
+// -----------------------------Варіант №1-------------------------
+
+// let likes = document.querySelectorAll("#like_products");
+// console.log(likes);
+
+// for(let i=0; i < likes.length; i++){
+//     likes[i].addEventListener("click", function(){
+
+//         if(likes[i].style.backgroundColor !=="rgb(44, 113, 184)"){
+//             likes[i].style.backgroundColor = "rgb(44, 113, 184)";
+//             likes[i].style.backgroundImage = "url(images/like_white.png)" ;
+
+//         } else{
+//             likes[i].style.backgroundColor = "rgba(255, 255, 255,0)";
+//             likes[i].style.backgroundImage = "url(images/like_blue.png)"
+//         }
+
+//     })
+
+// }
+
+
+
+// -----------------------------Варіант №2-------------------------
+
+
+
+let likes = document.querySelectorAll('#like_products');
+// console.log(likes);
+ for(let i=0; i<likes.length; i++){
+     likes[i].addEventListener('click',function(e){
+         let target = e.target;
+        //  console.log(target);
+         
+          if (target.style.backgroundColor !== 'rgb(44, 113, 184)'){
+            target.style.backgroundColor = 'rgb(44, 113, 184)';
+            target.style.backgroundImage = "url(images/like_white.png)"
+         }else{
+            target.style.backgroundColor = "rgba(255, 255, 255,0)";
+            target.style.backgroundImage = "url(images/like_blue.png)"
+            
+         }
+     })
+ }
